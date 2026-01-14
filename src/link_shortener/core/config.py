e2962508e6de0af656a0bb19c385bfd485d12e14
Maiz <maizekeen@internet.ru>
2026-01-15 02:21:55 +0500

    Changed and added settings to use hashed link shortening scheme, added settings to use postgresql in future in production

--- src/link_shortener/core/config.py
+++ src/link_shortener/core/config.py
@@ -1,16 +1,17 @@
 import os
+import secrets
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
 class BaseConfig:
-    SHORT_CODE_RANDOM_PART_LENGTH = 4
-    SHORT_CODE_ID_PART_LENGTH = 3
+    SHORT_CODE_LENGTH = 7
     DEBUG = True
 
     # Security App
-    SECRETS_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
+    SECRETS_KEY = os.environ.get('SECRET_KEY') or 'SECRET-KEY'
+    SHORT_CODE_SECRET_PEPPER = os.environ.get('SHORT_CODE_PEPPER') or secrets.token_hex(32)
 
     # App settings
     HOST = os.environ.get('HOST') or 'localhost'
@@ -22,27 +23,39 @@ class BaseConfig:
     MAX_URL_LENGTH = 2048
 
     # Limits
-    MAX_REQUESTS_PER_MINUTE = 60
+    MAX_REQUESTS_PER_MINUTE = 100
+    BATH_CREATE_LIMIT = 100 # Макс URL за один пакетный запрос
 
     # Database settings
     DATABASE_URL = os.environ.get('DATABASE_URL') or 'sqlite:///dev.db'
-    
-    # TODO add redis
+    if DATABASE_URL.startswith('postgresql://'):
+        # Настройки пула соединений для PostgreSQL
+        DATABASE_POOL_SIZE = int(os.environ.get('DATABASE_POOL_SIZE', 20))
+        DATABASE_MAX_OVERFLOW = int(os.environ.get('DATABASE_MAX_OVERFLOW', 10))
+        DATABASE_POOL_RECYCLE = int(os.environ.get('DATABASE_POOL_RECYCLE', 3600))
 
+    # TODO add redis for cache
+    # REDIS_URL = 
+    # CACHE_TTL = 
+
+    # TODO add monitoring
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
     
-
 class TestConfig:
     TESTING = True
-    SECRET_KEY = 'TEST-SECRET-KEY'
-
     DATABASE_URL = os.environ.get('DATABASE_URL_TEST') or 'sqlite:///test.db'
 
 class ProductionConfig(BaseConfig):
     DEBUG = False
 
     SECRETS_KEY = os.environ['SECRET_KEY']    
-    # TODO add database
+    
+    # POSTGRESQL IN PROD
+    DATABASE_URL = os.environ['DATABASE_URL']
+
+    # Настройки БД для высокой нагрузки
+    DATABASE_POOL_SIZE = int(os.environ.get('DATABASE_POOL_SIZE', 50))
+    DATABASE_MAX_OVERFLOW = int(os.environ.get('DATABASE_MAX_OVERFLOW', 20))
 
