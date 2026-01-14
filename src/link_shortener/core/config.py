060943cb0331e37ba5cfbac30bf190b4a9e06bb8
Maiz <maizekeen@internet.ru>
2026-01-14 12:45:23 +0500

    fix some syntax errors and added variables for database

--- src/link_shortener/core/config.py
+++ src/link_shortener/core/config.py
@@ -5,15 +5,17 @@ load_dotenv()
 
 
 class BaseConfig:
-    # Security App
-    SECRETS_KEY = os.environ.get('SECRETS_KEY') or 'dev-secret-key-change-in-production'
-
-    # App settings
-    BASE_URL = os.environ.get('BASE_URL') or 'http://localhost'
-    BASE_PORT = os.environ.get('BASE_PORT') or 5000
-    BASE_LINK = f'{BASE_URL}:{BASE_PORT}/'
     SHORT_CODE_RANDOM_PART_LENGTH = 4
     SHORT_CODE_ID_PART_LENGTH = 3
+    DEBUG = True
+
+    # Security App
+    SECRETS_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
+
+    # App settings
+    HOST = os.environ.get('HOST') or 'localhost'
+    PORT = os.environ.get('PORT') or 5000
+    BASE_LINK = f'{HOST}:{PORT}/'
 
     # Security URL
     ALLOWED_SHEMES = ['http', 'https', 'ftp']
@@ -22,24 +24,25 @@ class BaseConfig:
     # Limits
     MAX_REQUESTS_PER_MINUTE = 60
 
-    # TODO add DB
+    # Database settings
+    DATABASE_URL = os.environ.get('DATABASE_URL') or 'sqlite:///dev.db'
     # TODO add redis
 
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
-
-    # TODO add DEV DataBase
+    
 
 class TestConfig:
     TESTING = True
     SECRET_KEY = 'TEST-SECRET-KEY'
-    # TODO add Testing DataBase
+
+    DATABASE_URL = os.environ.get('DATABASE_URL_TEST') or 'sqlite:///test.db'
 
 class ProductionConfig(BaseConfig):
     DEBUG = False
 
-    SECRETS_KEY = os.environ['SECRETS_KEY']
+    SECRETS_KEY = os.environ['SECRET_KEY']
     
     # TODO add database
 
