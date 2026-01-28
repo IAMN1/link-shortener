3d03d982b047284fc94c0d0156188df080cc1fcb
Maiz <maizekeen@internet.ru>
2026-01-28 20:55:50 +0500

    added new variables for Redis cache  in configs

--- src/link_shortener/core/config.py
+++ src/link_shortener/core/config.py
@@ -46,9 +46,13 @@ class BaseConfig:
         DATABASE_MAX_OVERFLOW = int(os.environ.get('DATABASE_MAX_OVERFLOW', 10))
         DATABASE_POOL_RECYCLE = int(os.environ.get('DATABASE_POOL_RECYCLE', 3600))
 
-    # TODO add redis for cache
-    # REDIS_URL = 
-    # CACHE_TTL = 
+    # Redis cache settings
+    REDIS_ENABLED = os.environ.get('REDIS_ENABLED', True)
+    REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379/0')
+    REDIS_CACHE_TTL = int(os.environ.get('REDIS_CACHE_TTL', 24 * 60 * 60)) # 24h
+    REDIS_CACHE_TTL_STATS = int(os.environ.get('REDIS_CACHE_TTL_STATS', 5 * 60)) # 5m 
+
+    # add redis_cache_prefix ????
 
     # TODO add monitoring
 
@@ -58,6 +62,8 @@ class DevelopmentConfig(BaseConfig):
     LOG_LEVEL = logging.DEBUG
     LOG_TO_CONSOLE = True
     LOG_TO_FILE = False
+
+    REDIS_ENABLED = os.environ.get('REDIS_ENABLED', True)   
     
 class TestConfig:
     TESTING = True
@@ -67,6 +73,8 @@ class TestConfig:
     LOG_TO_FILE = False
 
     DATABASE_URL = os.environ.get('DATABASE_URL_TEST') or 'sqlite:///test.db'
+
+    REDIS_ENABLED = False
 
 class ProductionConfig(BaseConfig):
     DEBUG = False
@@ -84,4 +92,8 @@ class ProductionConfig(BaseConfig):
     # Настройки БД для высокой нагрузки
     DATABASE_POOL_SIZE = int(os.environ.get('DATABASE_POOL_SIZE', 50))
     DATABASE_MAX_OVERFLOW = int(os.environ.get('DATABASE_MAX_OVERFLOW', 20))
+
+    # Redis Cache
+    REDIS_ENABLED = os.environ.get('REDIS_ENABLED', True)
+    REDIS_URL = os.environ.get('REDIS_URL', 'redis://redis:6379/0')
 
