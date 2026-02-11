23503d4044198201f133c48b735a7fd2fb545b7d
Maiz <maizekeen@internet.ru>
2026-02-12 01:00:44 +0500

    refactor(infrastructure): Minor change to the model for storing links in the database: - Changed the database model name to something more logical - Fixed the last_accessed field

--- src/link_shortener/infrastructure/database/models.py
+++ src/link_shortener/infrastructure/database/models.py
@@ -1,28 +1,21 @@
 from datetime import datetime
-from sqlalchemy import Integer, String, func
+from sqlalchemy import Integer, String, func, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 from link_shortener.infrastructure.database.base import Base
 
-class TableURL(Base):
+class LinkModel(Base):
     """
     Модель SQLAlchemy для хранения сокращенных ссылок
-    Является имплементацией, а не доменной сущностью
-    
-    Vars:
-        original_url (str): Оригинальная ссылка
-        url_hash (str): Хэш URL
-        short_code (str): Сгенерированная короткая ссылка
-        clicks (int): Количество переходов по ссылке
     """
     __tablename__ = 'urls'
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     url_hash: Mapped[str] = mapped_column(String(64), unique=True, nullable=False, index=True)
-    original_url: Mapped[str] = mapped_column(String(2048), nullable=False,index=True)
-    short_code: Mapped[str] = mapped_column(String(10), unique=True, index=True)
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now, server_default=func.now())
+    original_url: Mapped[str] = mapped_column(String(2048), nullable=False)
+    short_code: Mapped[str] = mapped_column(String(10), unique=True, nullable=False, index=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, server_default=func.now())
     clicks: Mapped[int] = mapped_column(Integer, default=0)
-    last_accessed: Mapped[datetime] = mapped_column(default=datetime.now, server_default=func.now())
+    last_accessed: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, server_default=func.now())
 
 
 # TODO для будущего расширения функционала
