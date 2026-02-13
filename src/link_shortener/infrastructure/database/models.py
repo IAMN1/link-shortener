1e89d67df16c107cf471b73fc66903f5415bdc98
Maiz <maizekeen@internet.ru>
2026-02-13 05:31:52 +0500

    fix(infrastructure): fixing the fields of the data model by adding arguments to them to match the DTO

--- src/link_shortener/infrastructure/database/models.py
+++ src/link_shortener/infrastructure/database/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import uuid
 from sqlalchemy import Integer, String, func, DateTime
 from sqlalchemy.orm import Mapped, mapped_column
 from link_shortener.infrastructure.database.base import Base
@@ -9,13 +10,13 @@ class LinkModel(Base):
     """
     __tablename__ = 'urls'
 
-    id: Mapped[str] = mapped_column(String(36), primary_key=True)
+    id: Mapped[str] = mapped_column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     url_hash: Mapped[str] = mapped_column(String(64), unique=True, nullable=False, index=True)
     original_url: Mapped[str] = mapped_column(String(2048), nullable=False)
     short_code: Mapped[str] = mapped_column(String(10), unique=True, nullable=False, index=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, server_default=func.now())
     clicks: Mapped[int] = mapped_column(Integer, default=0)
-    last_accessed: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, server_default=func.now())
+    last_accessed: Mapped[datetime] = mapped_column(DateTime, default=None, nullable=True, onupdate=datetime.now)
 
 
 # TODO для будущего расширения функционала
