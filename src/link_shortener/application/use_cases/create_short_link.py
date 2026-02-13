6ef46dd3ae070173f93db7764107873de0edef04
Maiz <maizekeen@internet.ru>
2026-02-13 07:19:42 +0500

    feat (application): added collision handling

--- src/link_shortener/application/use_cases/create_short_link.py
+++ src/link_shortener/application/use_cases/create_short_link.py
@@ -1,13 +1,14 @@
 from dataclasses import dataclass
 from typing import Optional
 
-from src.link_shortener.application.dtos.responses import ShortLinkResponse
-from src.link_shortener.application.ports.cache.link_cache import LinkCache
-from src.link_shortener.application.ports.logger.logger import Logger
-from src.link_shortener.domain.entities.link import Link
-from src.link_shortener.domain.policies.shortening_policy import ShorteningPolicy
-from src.link_shortener.domain.repositories.link_repository import LinkRepository
-from src.link_shortener.domain.value_objects.original_url import OriginalUrl
+from ..dtos.responses import ShortLinkResponse
+from ..ports.cache.link_cache import LinkCache
+from ..ports.logger.logger import Logger
+from domain.entities.link import Link
+from domain.policies.shortening_policy import ShorteningPolicy
+from domain.repositories.link_repository import LinkRepository
+from domain.value_objects.original_url import OriginalUrl
+from domain.value_objects.short_code import ShortCode
 
 
 @dataclass
@@ -28,6 +29,7 @@ class CreateShortLinkUseCase:
     shortening_policy: ShorteningPolicy
     base_url: str
     logger: Optional[Logger] = None
+    max_collision_attempts: int = 5
 
     def execute(self, url: str) -> ShortLinkResponse:
         """
@@ -43,23 +45,23 @@ class CreateShortLinkUseCase:
             ValueError: Если URL невалидный
         """
         try:
-            # 1. создание VO c валидацией
+            # Создание VO c валидацией
             original_url = OriginalUrl(url)
 
             if self.logger:
                 self.logger.info('Starting short link creation', url=original_url.value[:50])
 
-            # 2. вычисление хэша для дедупликации
+            # Вычисление хэша для дедупликации
             url_hash = self.shortening_policy.calculate_hash(original_url)
 
-            # 3. Проверка кэша
+            # 1. Проверка кэша
             cached_link = self.cache.get_by_hash(url_hash)
             if cached_link:
                 if self.logger:
                     self.logger.debug('Cache hit for Url,', url=url[:50], hash=url_hash.value[:10])
                 return ShortLinkResponse.from_link(cached_link, base_url=self.base_url, from_cache=True)
             
-            # 4. Проверка репозитория
+            # 2. Проверка репозитория
             existing_link = self.repository.find_by_hash(url_hash)
             if existing_link:
                 if self.logger:
@@ -68,17 +70,17 @@ class CreateShortLinkUseCase:
                 self.cache.save(existing_link)
                 return ShortLinkResponse.from_link(link=existing_link, base_url=self.base_url, is_new=False, from_cache=False)
             
-            # 5. Генерация кода
-            short_code = self.shortening_policy.generate_code(original_url)
+            # 3. Генерация кода
+            short_code = self._generate_unique_code(original_url)
 
-            # 6 Создание доменной сущности
+            # Создание доменной сущности
             new_link = Link.create(
                 url_hash=url_hash,
                 short_code=short_code,
                 original_url=original_url
             )
 
-            # 7. Сохранение в репозиторий и кэш
+            # 4. Сохранение в репозиторий и кэш
             saved_link = self.repository.save(new_link)
             self.cache.save(saved_link)
 
@@ -97,7 +99,25 @@ class CreateShortLinkUseCase:
             if self.logger:
                 self.logger.error('Validation failed', url=url[:50], error=str(e))
             raise ValueError(f'Invalid URL {str(e)}')
+        
         except Exception as e:
             if self.logger:
                 self.logger.error('Error creating short link', error=str(e))
             raise
+    
+    def _generate_unique_code(self, original_url: OriginalUrl) -> ShortCode:
+        """Генерация уникального кода с проверкой коллизии в репозитории"""
+        attempt = 0
+        current_url = original_url
+        while attempt < self.max_collision_attempts:
+            code = self.shortening_policy.generate_code(current_url)
+            existing = self.repository.find_by_code(code)
+            if not existing or existing.url_hash == self.shortening_policy.calculate_hash(original_url):
+                
+                # коллизии нет или это та же самая ссылка (не должно произойти, но на всякий случай)
+                return code
+            
+            # Колизия
+            attempt += 1
+            current_url = OriginalUrl(f"{original_url.value}#collision_{attempt}")
+        raise RuntimeError("Failed to generate unique short code after multiple attempts")
