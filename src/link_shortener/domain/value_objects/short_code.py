b78361725a98cf4e86bf843f6129fa6bb77034c2
Maiz <maizekeen@internet.ru>
2026-02-14 14:53:09 +0500

    style(domain): formatting with black

--- src/link_shortener/domain/value_objects/short_code.py
+++ src/link_shortener/domain/value_objects/short_code.py
@@ -1,5 +1,5 @@
-from dataclasses import dataclass
 import re
+from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
@@ -8,20 +8,21 @@ class ShortCode:
     Valie object для короткого кода
     Не изменяемый, содержит валидацию
     """
+
     value: str
 
     def __post_init__(self):
         """Валидация при создании объекта"""
-        if not re.match(r'^[a-zA-Z0-9_-]{6,10}$', self.value):
+        if not re.match(r"^[a-zA-Z0-9_-]{6,10}$", self.value):
             raise ValueError(
                 f"Invalid short code format: {self.value}. "
                 f"Must be 6-10 alphanumeric characters."
             )
-    
+
     def __str__(self) -> str:
         return self.value
-    
+
     @classmethod
-    def create(cls, value: str) -> 'ShortCode':
+    def create(cls, value: str) -> "ShortCode":
         """Фабричный метод с явной валидацией"""
         return cls(value)
