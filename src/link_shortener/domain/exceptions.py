189ff7884dfc63c4947eea659a585bee7df6a0d1
Maiz <maizekeen@internet.ru>
2026-02-14 14:53:38 +0500

    style(domain): formatting with black

--- src/link_shortener/domain/exceptions.py
+++ src/link_shortener/domain/exceptions.py
@@ -1,5 +1,6 @@
 class DomainError(Exception):
     """Базовые исключения доменного слоя"""
+
     def __init__(self, message: str, code: str = "DOMAIN_ERROR"):
         self.message = message
         self.code = code
@@ -18,7 +19,7 @@ class LinkNotFoundError(DomainError):
     """Ссылка не найдена"""
 
     def __init__(self, short_code: str = None):
-        message = 'Link not found'
+        message = "Link not found"
         if short_code:
-            message = f'Link with code ({short_code}) not found'
+            message = f"Link with code ({short_code}) not found"
         super().__init__(message, "LINK_NOT_FOUND")
