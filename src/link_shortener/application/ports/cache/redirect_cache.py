0fac3991ab74ec2053e22e199535fc1353c32a61
Maiz <maizekeen@internet.ru>
2026-02-14 14:57:58 +0500

    feat(application): Added an interface for auditing logs and formatting with black and fix imports

--- src/link_shortener/application/ports/cache/redirect_cache.py
+++ src/link_shortener/application/ports/cache/redirect_cache.py
@@ -1,12 +1,12 @@
 from abc import ABC, abstractmethod
 from typing import Optional
 
-from src.link_shortener.domain.value_objects.short_code import ShortCode
+from link_shortener.domain import ShortCode
 
 
 class RedirectCache(ABC):
     """Интерфейс для кэширования URL для быстрого редиректа"""
-    
+
     @abstractmethod
     def get_original_url(self, short_code: ShortCode) -> Optional[str]:
         """Получение оригинальной ссылки"""
@@ -16,7 +16,7 @@ class RedirectCache(ABC):
     def save_original_url(self, short_code: ShortCode, original_url: str) -> None:
         """Сохранение оригинальной ссылки в кэш для редиректа"""
         pass
-    
+
     # @abstractmethod
     # def delete_original_url(self, short_code: ShortCode) -> None:
     #     """Удаление URL для редиректа"""
