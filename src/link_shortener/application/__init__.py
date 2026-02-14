157f1d09c329ec9d8f991416d632ffec34d60941
Maiz <maizekeen@internet.ru>
2026-02-14 15:05:33 +0500

    feat(application): filled out the init.py for re-export

---
+++ src/link_shortener/application/__init__.py
@@ -0,0 +1,37 @@
+from .dtos.responses import (BatchCreateResponse, BatchItemResponse,
+                             ExtendedLinkInfoResponse, ServiceStatsResponse,
+                             ShortLinkResponse, StatsItemResponse)
+from .ports.cache.link_cache import LinkCache
+from .ports.cache.link_service_stats_cache import StatsCache
+from .ports.cache.redirect_cache import RedirectCache
+from .ports.logger.audit import AuditLogger
+from .ports.logger.logger import ContextLogger, Logger
+from .services.link_service import LinkService
+from .use_cases.batch_create_links import BatchCreateLinksUseCase
+from .use_cases.create_short_link import CreateShortLinkUseCase
+from .use_cases.get_link_info import (GetExtendLinkInfoUseCase,
+                                      GetLinkInfoUseCase)
+from .use_cases.get_service_stats import GetServiceStatsUseCase
+from .use_cases.redirect_link import RedirectLinkUseCase
+
+__all__ = [
+    "ShortLinkResponse",
+    "BatchItemResponse",
+    "BatchCreateResponse",
+    "StatsItemResponse",
+    "ServiceStatsResponse",
+    "ExtendedLinkInfoResponse",
+    "LinkCache",
+    "StatsCache",
+    "RedirectCache",
+    "AuditLogger",
+    "Logger",
+    "ContextLogger",
+    "LinkService",
+    "BatchCreateLinksUseCase",
+    "CreateShortLinkUseCase",
+    "GetLinkInfoUseCase",
+    "GetExtendLinkInfoUseCase",
+    "GetServiceStatsUseCase",
+    "RedirectLinkUseCase",
+]
