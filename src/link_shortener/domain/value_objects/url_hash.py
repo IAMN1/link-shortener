df37a82dcfa7fc967312a1a044c9e89224fd3e4b
Maiz <maizekeen@internet.ru>
2026-02-14 14:53:22 +0500

    style(domain): formatting with black

--- src/link_shortener/domain/value_objects/url_hash.py
+++ src/link_shortener/domain/value_objects/url_hash.py
@@ -1,5 +1,5 @@
-from dataclasses import dataclass
 import re
+from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
@@ -8,14 +8,14 @@ class UrlHash:
     Value object для хэша URL
     Используется для дедупликации
     """
+
     value: str
 
     def __post_init__(self):
-        if not re.match(r'^[a-f0-9]{64}$', self.value):
+        if not re.match(r"^[a-f0-9]{64}$", self.value):
             raise ValueError(
-                f"Invalid hash format: {self.value}. "
-                f"Must be 64 hex characters."
+                f"Invalid hash format: {self.value}. " f"Must be 64 hex characters."
             )
-    
+
     def __str__(self) -> str:
         return self.value
