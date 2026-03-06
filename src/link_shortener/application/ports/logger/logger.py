224c8ec73dbe09e3e448c8e5a411ee2611420826
Maiz <maizekeen@internet.ru>
2026-03-06 21:10:58 +0500

    refactor(logger): remove unused ContextLogger and with_context method

--- src/link_shortener/application/ports/logger/logger.py
+++ src/link_shortener/application/ports/logger/logger.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any, Dict, Optional
+from typing import Any, Optional
 
 
 class Logger(ABC):
@@ -48,44 +48,3 @@ class Logger(ABC):
         """
         pass
 
-    def with_context(self, **context: Any) -> "Logger":
-        """
-        Return a new logger that includes the given context 
-            in all subsequent log calls.
-
-        Args:
-            **context: Key-value pairs to add to log entries.
-
-        Returns:
-            A logger instance with bound context.
-        """
-        return ContextLogger(self, context)
-
-
-class ContextLogger(Logger):
-    """
-    Logger decorator that adds static context to every log message.
-
-    Used internally by Logger.with_context().
-    """
-
-    def __init__(self, inner_logger: Logger, context: Dict[str, Any]):
-        self.inner_logger = inner_logger
-        self.context = context
-
-    def debug(self, message: str, **kwargs: Any) -> None:
-        self.inner_logger.debug(message, **{**self.context, **kwargs})
-
-    def info(self, message: str, **kwargs: Any) -> None:
-        self.inner_logger.info(message, **{**self.context, **kwargs})
-
-    def warning(self, message: str, **kwargs: Any) -> None:
-        self.inner_logger.warning(message, **{**self.context, **kwargs})
-
-    def error(self, message: str, **kwargs: Any) -> None:
-        self.inner_logger.error(message, **{**self.context, **kwargs})
-
-    def exception(
-        self, message: str, exc_info: Optional[Exception] = None, **kwargs: Any
-    ) -> None:
-        self.inner_logger.exception(message, exc_info, **{**self.context, **kwargs})
