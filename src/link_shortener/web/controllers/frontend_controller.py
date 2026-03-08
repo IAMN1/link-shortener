9c89850b97f6e7919584fbdadded06622580fbf4
Maiz <maizekeen@internet.ru>
2026-03-08 16:36:03 +0500

    refactor(frontend-controller): The FrontendController has been updated: - The old /info/<code> and /extended/<code> routes now redirect to the homepage with the mode and code parameters. - The /api/docs route has been added (temporarily redirects to swagger.io).

--- src/link_shortener/web/controllers/frontend_controller.py
+++ src/link_shortener/web/controllers/frontend_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, url_for
 from link_shortener.application import LinkService
 
 
@@ -21,24 +21,24 @@ class FrontendController:
 
     def _register_routes(self):
         self.bp.add_url_rule('/', view_func=self.index, methods=['GET'])
-        self.bp.add_url_rule('/info/<short_code>', view_func=self.info, methods=['GET'])
-        self.bp.add_url_rule('/info/<short_code>/extended', view_func=self.extended_info, methods=['GET'])
+        self.bp.add_url_rule('/info/<short_code>', view_func=self.info_redirect, methods=['GET'])
+        self.bp.add_url_rule('/extended/<short_code>', view_func=self.extended_info_redirect, methods=['GET'])
         self.bp.add_url_rule('/stats', view_func=self.stats, methods=['GET'])
+        self.bp.add_url_rule('/api/docs', view_func=self.api_docs, methods=['GET'])
 
     def index(self):
         return render_template("index.html")
 
-    def info(self, short_code: str):
+    def info_redirect(self, short_code: str):
+        return redirect(url_for('frontend.index', mode='info', code=short_code))
 
-        info = self.link_service.get_link_info(short_code)
-        return render_template('info.html', link=info)
-
-    def extended_info(self, short_code: str):
-        """Render extended information page for a short link."""
-        info = self.link_service.get_extended_link_info(short_code)
-        return render_template('extended_info.html', link=info)
+    def extended_info_redirect(self, short_code: str):
+        return redirect(url_for('frontend.index', mode='extended', code=short_code))
 
     def stats(self):
-
         stats = self.link_service.get_service_stats()
         return render_template('stats.html', stats=stats)
+
+    def api_docs(self):
+        # Замените на реальную документацию (например, Swagger UI)
+        return redirect("https://swagger.io")
