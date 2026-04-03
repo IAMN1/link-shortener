1d59b16ca347e48539a32f5d640de9fe67cf8d4f
Maiz <maizekeen@internet.ru>
2026-04-04 04:10:11 +0500

    refactor(link_service): update imports after use cases reorganization

--- src/link_shortener/application/services/link_service.py
+++ src/link_shortener/application/services/link_service.py
@@ -4,10 +4,10 @@ from typing import List
 from link_shortener.application.context import RequestContext
 from link_shortener.application.dtos.responses import BatchCreateResponse, ExtendedLinkInfoResponse, ServiceStatsResponse, ShortLinkResponse
 from link_shortener.application.use_cases.batch.batch_create_links import BatchCreateLinksUseCase
-from link_shortener.application.use_cases.create_short_link import CreateShortLinkUseCase
-from link_shortener.application.use_cases.get_link_info import GetExtendLinkInfoUseCase, GetLinkInfoUseCase
-from link_shortener.application.use_cases.get_service_stats import GetServiceStatsUseCase
-from link_shortener.application.use_cases.redirect_link import RedirectLinkUseCase
+from link_shortener.application.use_cases.links.create_short_link import CreateShortLinkUseCase
+from link_shortener.application.use_cases.links.get_link_info import GetExtendLinkInfoUseCase, GetLinkInfoUseCase
+from link_shortener.application.use_cases.links.redirect_link import RedirectLinkUseCase
+from link_shortener.application.use_cases.stats.get_service_stats import GetServiceStatsUseCase
 
 
 @dataclass
